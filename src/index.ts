85bd7a015b90da1298ee83a96f7d2a51597ef8cc
21120569 <21120569@student.hcmus.edu.vn>
2024-05-04 22:09:58 +0700

    fix chapter

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import { FileExtensionManager } from './models/FileExtension/FileExtensionManage
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // require('ts-node').register();
 // require('./index.ts');
-import axios from 'axios';
+
 const port = 3000;
 const host = '127.0.0.1'; //'10.0.2.2';////'localhost';
 const app = express();
