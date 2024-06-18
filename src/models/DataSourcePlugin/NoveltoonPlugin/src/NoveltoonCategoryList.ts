c1ce1c173e9a87220975f42a92aeef3bd5926080
21120612 <nmt6465@gmail.com>
2024-06-18 11:36:35 +0700

    noveltoon and tanthuvien

--- src/models/DataSourcePlugin/NoveltoonPlugin/src/NoveltoonCategoryList.ts
+++ src/models/DataSourcePlugin/NoveltoonPlugin/src/NoveltoonCategoryList.ts
@@ -1,4 +1,6 @@
+import Cheerio from 'cheerio';
 import { ICategoryList } from '~/models/DataSourceManagement/ICategoryList';
+import { Category } from '~/models/Interfaces/Category';
 
 export default class NoveltoonCategoryList implements ICategoryList {
   name: string;
@@ -13,7 +15,38 @@ export default class NoveltoonCategoryList implements ICategoryList {
   getBaseUrl(): string {
     return this.baseUrl;
   }
-  categoryList(type?: string | undefined) {
-    throw new Error('Method not implemented.');
+  private getCategoryList = (html: string) => {
+    const $ = Cheerio.load(html);
+    const result: Category[] = [];
+    const length = $('.channel-list a').length;
+    $('.channel-list a').each((index, element) => {
+      if (index == 0) return;
+      const content = $(element).find('.channel').text().trim();
+      const href = $(element).attr('href') || '';
+      result.push({
+        content,
+        //host: `${this.getBaseUrl()}`,
+        href
+      });
+    });
+    return result;
+  };
+  async categoryList(): Promise<Category[]> {
+    let result: Category[] = [];
+    try {
+      const searchString: string = `${this.getBaseUrl()}vi/genre/novel`;
+      const response = await fetch(searchString, {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'PostmanRuntime/7.39.0'
+        }
+      });
+      const html = await response.text();
+      result = this.getCategoryList(html);
+    } catch (err) {
+      console.log(err);
+    }
+
+    return result;
   }
 }
--- src/models/DataSourcePlugin/NoveltoonPlugin/src/NoveltoonSearchStory.ts
+++ src/models/DataSourcePlugin/NoveltoonPlugin/src/NoveltoonSearchStory.ts
@@ -1,58 +1,107 @@
-import { ICategoryList } from "~/models/DataSourceManagement/ICategoryList";
-import { IContentStory } from "~/models/DataSourceManagement/IContentStory";
-import { IDataSourcePlugin } from "~/models/DataSourceManagement/IDataSourcePlugin";
-import { IDetailStory } from "~/models/DataSourceManagement/IDetailStory";
-import { IListStoryStrategy } from "~/models/DataSourceManagement/IListStoryStrategy";
-import { ISearchStory } from "~/models/DataSourceManagement/ISearchStory";
+import Cheerio from 'cheerio';
+import { ISearchStory } from '~/models/DataSourceManagement/ISearchStory';
+import { Story } from '~/models/Interfaces/Story';
 
-export default class NoveltoonPlugin implements IDataSourcePlugin {
-    name: string;
-    listStory: IListStoryStrategy;
-    listCategory: ICategoryList;
-    contentChapter: IContentStory;
-    storyDetail: IDetailStory;
-    searcher: ISearchStory;
-    clone(name: string): IDataSourcePlugin {
-        throw new Error("Method not implemented.");
+export default class NoveltoonSearchStory implements ISearchStory {
+  name: string;
+  baseUrl: string;
+  public constructor(url: string) {
+    this.name = 'NoveltoonCategoryList';
+    this.baseUrl = url;
+  }
+  clone(): ISearchStory {
+    return new NoveltoonSearchStory(this.baseUrl);
+  }
+  getBaseUrl(): string {
+    return this.baseUrl;
+  }
+  private removeVietnameseAccents(str: string): string {
+    return str
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .replace(/đ/g, 'd')
+      .replace(/Đ/g, 'D');
+  }
+  private convertString(input: string): string {
+    // Chuyển đổi chuỗi về chữ thường
+    const lowerCaseStr = input.toLowerCase();
+
+    // Bỏ dấu tiếng Việt
+    const noAccentsStr = this.removeVietnameseAccents(lowerCaseStr);
+
+    // Thay thế các dấu cách bằng dấu gạch ngang
+    const kebabCaseStr = noAccentsStr.split(' ').join('-');
+    return kebabCaseStr;
+  }
+
+  private cutStringTitle(input: string): string {
+    const result = input.split('/vi/');
+    return result[1];
+  }
+  private convertStringToCategoryList(
+    input: string,
+    separate: string
+  ): { content: string; href: string }[] {
+    return input
+      .split(separate)
+      .map((el, index) => {
+        const trimmedCategory = el.trim();
+        return {
+          content: trimmedCategory,
+          href: this.convertString(trimmedCategory)
+        };
+      })
+      .filter((el) => el.content.toLocaleLowerCase() !== 'đã full');
+  }
+  private getStoryForSearch = (html: string) => {
+    const result: Story[] = [];
+    const $ = Cheerio.load(html);
+    $('.recommend-comics .recommend-item ').each((index, element) => {
+      const name = $(element).find('.recommend-comics-title').text().trim();
+      const link = $(element).find('a').attr('href') || '';
+      const cover = $(element).find('a .comics-image img').attr('data-src') || '';
+      const description = $(element).find('.comics-type span').text().trim();
+      const category = this.convertStringToCategoryList(description, '/');
+      const title = this.cutStringTitle(link);
+      const story: Story = {
+        name,
+        link,
+        cover,
+        description: description,
+        author: 'no information',
+        categoryList: category,
+        title,
+        host: `${this.getBaseUrl()}`,
+        authorLink: 'no information',
+        view: 'no information'
+      };
+      result.push(story);
+    });
+    return result;
+  };
+  async search(title: string, page?: string | undefined, category?: string | undefined) {
+    const searchString: string = `${this.getBaseUrl()}/vi/search?word=${title}`;
+    let result: Story[] = [];
+    try {
+      const response = await fetch(searchString, {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'PostmanRuntime/7.39.0'
+        }
+      });
+      const html = await response.text();
+      result = this.getStoryForSearch(html);
+      if (category) {
+        result = result.filter((story) => {
+          return story.categoryList.some((el) => {
+            return el.content === category;
+          });
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      return null;
     }
-    getBaseUrl(): string {
-        throw new Error("Method not implemented.");
-    }
-    search(title: string, page?: string | undefined, category?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    detailStory(title: string) {
-        throw new Error("Method not implemented.");
-    }
-    contentStory(title: string, chap?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    categoryList(type?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    chapterList(title: string, page?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    selectStoryList(type: string, limiter?: number | undefined, page?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    home() {
-        throw new Error("Method not implemented.");
-    }
-    changeDetailStoryToThisDataSource(title: string) {
-        throw new Error("Method not implemented.");
-    }
-    changeContentStoryToThisDataSource(title: string, chap?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    newestStoryAtCategory(category: string, limiter?: number | undefined, page?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    fullStoryAtCategory(category: string, limiter?: number | undefined, page?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    hotStoryAtCategory(category: string, limiter?: number | undefined, page?: string | undefined) {
-        throw new Error("Method not implemented.");
-    }
-    
+    return result;
+  }
 }
