7100e6f3b79d1d9ae472c18e899ae4aa809cb519
21120569 <21120569@student.hcmus.edu.vn>
2024-06-28 16:51:13 +0700

    add mp3 plugin

--- src/converter/wav.ts
+++ src/converter/wav.ts
@@ -1,16 +1,81 @@
-import text2wav = require('text2wav');
 import * as fs from 'fs';
-
+import { env } from 'process';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+interface APIFPTAIConvertTextToSpeech {
+  error?: number;
+  async?: string;
+  request_id?: string;
+  message?: string;
+}
 export async function createFile(title: string, chapter: string, content: string): Promise<any> {
-  const filePath = `downloadedFile/${title}_${chapter}.wav`;
+  const filePath = `downloadedFile/${title}_${chapter}.mp3`;
+
+  const apiKey = process.env.API_CONVERT_TEXT_TO_SPEECH_KEY || '';
+  const apiUrl = 'https://api.fpt.ai/hmi/tts/v5';
+  const speed = '';
+  const voice = 'ngoclam';
+  // const payload = Buffer.from(
+  //   'có rất nhiều truyền kỳ và câu chuyện huyền bí xoay quanh nó.',
+  //   'utf-8'
+  // );
+  const payload = JSON.stringify('nhiều');
+  const headers: Record<string, string> = {
+    'api-key': apiKey,
+    speed: speed,
+    voice: voice
+  };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: payload
+  };
+  //console.log('requestOptions:', requestOptions);
+
   try {
-    const out = await text2wav(
-      'Tao là tiến. Về kết hôn âm dương, có rất nhiều truyền kỳ và câu chuyện huyền bí xoay quanh nó.Trung Quốc thời đó chuộng loại kết hôn này coi như một cách để thành toàn vong linh người đã khuất.Có điều ở hiện đại, không mấy ai còn làm điều này nữa.!',
-      { voice: 'vi-vn-x-south+mb-cn1', wordGap: 300, speed:200  }
-    );
-    fs.writeFileSync(filePath, out);
-    console.log('save wav');
-    return filePath;
+    const response = await fetch(apiUrl, requestOptions);
+
+    //console.log('response:', response);
+    if (response.ok) {
+      const text = await response.text();
+      console.log('text: ', text);
+      const dataResponse: APIFPTAIConvertTextToSpeech = JSON.parse(text);
+
+      if (dataResponse.error !== undefined && dataResponse.error == 0 && dataResponse.async) {
+        const audioResponse = await fetch(dataResponse.async); //file url in AWS
+
+        if (audioResponse.ok) {
+          const buffer = await audioResponse.arrayBuffer();
+          fs.writeFileSync(filePath, Buffer.from(buffer));
+          console.log('saved .mp3!');
+          return filePath;
+        } else {
+          console.log('Cannot download audio file');
+        }
+      } else {
+        console.log('Error when API convert text to speech');
+      }
+    } else {
+      console.log('fetch api fpt AI fail!');
+    }
+  } catch (err) {
+    console.log('Error writing file:', err);
+    return null;
+  }
+}
+export async function downloadFile(fileUrl: string) {
+  const filePath = `downloadedFile/title_chapter.mp3`;
+  try {
+    const audioResponse = await fetch(fileUrl);
+    if (audioResponse.ok) {
+      const buffer = await audioResponse.arrayBuffer();
+      fs.writeFileSync(filePath, Buffer.from(buffer));
+      console.log('saved .mp3!');
+      return filePath;
+    } else {
+      console.log('Cannot download audio file');
+    }
   } catch (err) {
     console.log('Error writing file:', err);
     return null;
